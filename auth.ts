4f1ced207e238cd59e53f1953a0ed8d01de66a32
desi-10 <desmondkudjuh99@gmail.com>
2025-11-12 02:07:21 +0000

    updated envs

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { betterAuth } from "better-auth";
 import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL as string,
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: false,
